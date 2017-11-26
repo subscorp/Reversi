ebadff463d82c334163f230562de6b939256b510
Ori Hirshfeld <ori81510@gmail.com>
2017-11-26 23:54:44 +0200

    working smart AI with prints for debuging

--- Reversi/AIPlayer.cpp
+++ Reversi/AIPlayer.cpp
@@ -42,11 +42,13 @@ bool AIPlayer::canDoMove(Board *board)
 //let the player make a move
 void AIPlayer::makeMove(Board *board, int* move)
 {
-	int numWhites, numBlacks;
+	int numMovesO;
+	int numMovesX;
 	int score;
 	int max, min;
-	int maxIndex;
+	int minIndex;
 	Board boardCopy = *board;
+	Board boardCopy2;
 
 	//cout << "testing boardCopy by placing something at 7,7 " << endl;
 	//boardCopy.setCell('X',7,7);
@@ -61,32 +63,75 @@ void AIPlayer::makeMove(Board *board, int* move)
 	//cout <<"original board stats: numBlacks is " << numBlacks << " numWhites is " << numWhites <<endl;
 	//cout << "after check" << endl;
 
-	logic->getPossibleMoves(color, board, &possibleMoves);
+	//logic->getPossibleMoves(color, board, &possibleMoves);
 	if (canDoMove(board))
 	{
-		for(int i=0; i<possibleMoves.size();i++)
+		logic->getPossibleMoves(color, board, &possibleMoves);
+		numMovesO = possibleMoves.size();
+
+		for(int i=0; i<numMovesO;i++)
 		{
-			//cout << "checking state for possibleMove " << i << ":" << endl;
+			possibleMoves.clear();
+			logic->getPossibleMoves(color, board, &possibleMoves);
+
+			cout << "checking state for possibleMove " << i << ":" << endl;
 			boardCopy.setCell('O',possibleMoves[i].x,possibleMoves[i].y);
 			boardCopy.flip(possibleMoves[i].x,possibleMoves[i].y,'O');
+			boardCopy.printBoard();
+			possibleMoves.clear();
+			logic->getPossibleMoves('X',&boardCopy,&possibleMoves);
+			numMovesX = possibleMoves.size();
+			cout << "possible moves for X given this move are: " << endl;
+			for(int j=0;j<numMovesX;j++)
+			{
+				boardCopy2 = boardCopy;
+				cout << possibleMoves[j].x << " " << possibleMoves[j].y << endl;
+				boardCopy2.setCell('X',possibleMoves[j].x, possibleMoves[j].y);
+				boardCopy2.flip(possibleMoves[j].x, possibleMoves[j].y,'X');
+				score = boardCopy2.getNumBlacks() - boardCopy2.getNumWhites();
+				if(j == 0)
+					max = score;
+
+				if(score > max)
+					max = score;
+				cout << "current max: " << max << endl;
+
+			}
+			if(numMovesX == 0)
+			{
+				max = boardCopy.getNumBlacks() - boardCopy.getNumWhites();
+			}
+			if(i == 0)
+			{
+				min = max;
+				minIndex = 0;
+			}
+			if(max < min)
+			{
+				min = max;
+				minIndex = i;
+			}
+			cout << " current min: " << min << endl;
 			//boardCopy.printBoard();
-			score = boardCopy.getNumWhites() - boardCopy.getNumBlacks();
+			//score = boardCopy.getNumWhites() - boardCopy.getNumBlacks();
 			//cout << "the score is: " << score << endl;
 			boardCopy = *board;
-			if(score > max)
-			{
-				max = score;
-				maxIndex = i;
-			}
+			//if(score > max)
+			//{
+				//max = score;
+				//maxIndex = i;
+			//}
 		}
+		possibleMoves.clear();
+		logic->getPossibleMoves(color, board, &possibleMoves);
 
 		//temp commands
-		move[0] = possibleMoves[maxIndex].x;
-		move[1] = possibleMoves[maxIndex].y;
+		move[0] = possibleMoves[minIndex].x;
+		move[1] = possibleMoves[minIndex].y;
 		cout << "AI choices are: ";
 		logic->printPossibleMoves(color,board);
-		cout << "AI played " << '(' <<possibleMoves[maxIndex].x
-				<<',' <<possibleMoves[maxIndex].y << ')' << endl;
+		cout << "AI played " << '(' <<possibleMoves[minIndex].x
+				<<',' <<possibleMoves[minIndex].y << ')' << endl;
 		possibleMoves.clear();
 	}
 	else
